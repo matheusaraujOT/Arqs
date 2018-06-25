86eac6565ffd4f4bcf401a3f2024f71bd5a99e98
Matheus Henrique <matheus.araujo94@icloud.com>
2018-06-25 19:21:56 -0300

    Laboratorio 4

---
+++ loja/src/test/java/br/unibh/loja/negocio/TesteServicoCliente.java
@@ -0,0 +1,105 @@
+package br.unibh.loja.negocio;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.util.Date;
+import java.util.logging.Logger;
+
+import javax.inject.Inject;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.jboss.shrinkwrap.resolver.api.maven.Maven;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+
+import br.unibh.loja.entidades.Categoria;
+import br.unibh.loja.entidades.Cliente;
+import br.unibh.loja.entidades.Produto;
+import br.unibh.loja.util.Resources;
+
+@RunWith(Arquillian.class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class TesteServicoCliente {
+	@Deployment
+	public static Archive<?> createTestArchive() {
+		File[] files = Maven.resolver().loadPomFromFile("pom.xml").importRuntimeDependencies().resolve()
+				.withTransitivity().asFile();
+		// Cria o pacote que vai ser instalado no Wildfly para realizacao dos testes
+		return ShrinkWrap.create(WebArchive.class, "testeloja.war")
+				.addClasses(Cliente.class, Produto.class, Categoria.class, Resources.class, DAO.class,
+						ServicoCliente.class)
+				.addAsLibraries(files)
+				.addAsResource("META-INF/persistence.xml", "META-INF/persistence.xml")
+				.addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml");
+	}
+
+	// Realiza as injecoes com CDI
+	@Inject
+	private Logger log;
+	@Inject
+	private ServicoCliente sc;
+
+	@Test
+	public void teste01_inserirSemErro() throws Exception {
+		log.info("============> Iniciando o teste " + Thread.currentThread().getStackTrace()[1].getMethodName());
+		Cliente o = new Cliente(null, "wesley", "wesleysls", "12345", "Standard", "10352390603", "(99)9999-9999",
+				"wesley.sls21@gmail.com", new Date(), new Date());
+		sc.insert(o);
+		Cliente aux = (Cliente) sc.findByName("wesley").get(0);
+		assertNotNull(aux);
+		log.info("============> Finalizando o teste " + Thread.currentThread().getStackTrace()[1].getMethodName());
+	}
+
+	@Test
+	public void teste02_inserirComErro() throws Exception {
+		log.info("============> Iniciando o teste " + Thread.currentThread().getStackTrace()[1].getMethodName());
+		try {
+			Cliente o = new Cliente(null, "wesley", "wesleysls", "12345", "perfil", "103.523.906-03@", "(99)9999-9999",
+					"wesley.sls21@gmail.com", new Date(), new Date());
+			sc.insert(o);
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			assertTrue(checkString(e, "o cliente precisa ser criado com o perfil Standard"));
+		}
+		log.info("============> Finalizando o teste " + Thread.currentThread().getStackTrace()[1].getMethodName());
+	}
+
+	@Test
+	public void teste03_atualizar() throws Exception {
+		log.info("============> Iniciando o teste " + Thread.currentThread().getStackTrace()[1].getMethodName());
+		Cliente o = (Cliente) sc.findByName("wesley").get(0);
+		o.setNome("wesley modificado");
+		sc.update(o);
+		Cliente aux = (Cliente) sc.findByName("wesley modificado").get(0);
+		assertNotNull(aux);
+		log.info("============> Finalizando o teste " + Thread.currentThread().getStackTrace()[1].getMethodName());
+	}
+
+	@Test
+	public void teste04_excluir() throws Exception {
+		log.info("============> Iniciando o teste " + Thread.currentThread().getStackTrace()[1].getMethodName());
+		Cliente o = (Cliente) sc.findByName("wesley").get(0);
+		sc.delete(o);
+		assertEquals(0, sc.findByName("wesley modificado").size());
+		log.info("============> Finalizando o teste " + Thread.currentThread().getStackTrace()[1].getMethodName());
+	}
+
+	private boolean checkString(Throwable e, String str) {
+		if (e.getMessage().contains(str)) {
+			return true;
+		} else if (e.getCause() != null) {
+			return checkString(e.getCause(), str);
+		}
+		return false;
+	}
+}
